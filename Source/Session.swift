1cc8e917585e2b1a4125c469f7588b4c954c2ebf
Jon Shier <jon@jonshier.com>
2019-07-14 23:58:01 -0400

    Add cancel API, setup tests.

--- Source/Session.swift
+++ Source/Session.swift
@@ -193,6 +193,22 @@ open class Session {
         finishRequestsForDeinit()
         session.invalidateAndCancel()
     }
+    
+    // MARK: - Cancellation
+    
+    /// Cancel all active `Request`s, optionally calling a completion handler when complete.
+    ///
+    /// - Note: This is an asynchronous operation and does not block the creation of future `Request`s. Cancelled
+    ///         `Request`s may not cancel immediately due internal work, and may not cancel at all if they are close to
+    ///         completion when cancelled.
+    ///
+    /// - Parameter completion: Closure to be called when all `Request`s have been cancelled.
+    public func cancelAllRequests(completion: (() -> Void)? = nil) {
+        rootQueue.async {
+            self.activeRequests.forEach { $0.cancel() }
+            completion?()
+        }
+    }
 
     // MARK: - DataRequest
 
